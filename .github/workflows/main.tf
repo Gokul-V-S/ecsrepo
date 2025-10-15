bd0fa6ab33338ddd926a200d79e5b20a871ea7a3
Gokul-V-S <67053919+Gokul-V-S@users.noreply.github.com>
2025-10-15 07:13:56 +0530

    Create main.tf

---
+++ .github/workflows/main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region     = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+resource "aws_instance" "example" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "GitHub-Actions-EC2"
+  }
+}
+
+variable "aws_region"     {}
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "ami_id" {
+  default = "ami-0c02fb55956c7d316" # Amazon Linux 2 AMI (us-east-1)
+}
+variable "instance_type" {
+  default = "t2.micro"
+}
